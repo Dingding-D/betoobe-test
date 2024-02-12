1f64429dc996d22884c3b62487507363c7268992
Damien Dallon <dallon.damien@gmail.com>
2024-02-12 15:24:53 +0100

    Add Acivity entity

---
+++ src/Entity/Activity.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ActivityRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ActivityRepository::class)]
+class Activity
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $startAt = null;
+
+    #[ORM\Column]
+    private ?\DateTimeImmutable $endAt = null;
+
+    #[ORM\Column]
+    private ?int $participantMaxCount = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getStartAt(): ?\DateTimeImmutable
+    {
+        return $this->startAt;
+    }
+
+    public function setStartAt(\DateTimeImmutable $startAt): static
+    {
+        $this->startAt = $startAt;
+
+        return $this;
+    }
+
+    public function getEndAt(): ?\DateTimeImmutable
+    {
+        return $this->endAt;
+    }
+
+    public function setEndAt(\DateTimeImmutable $endAt): static
+    {
+        $this->endAt = $endAt;
+
+        return $this;
+    }
+
+    public function getParticipantMaxCount(): ?int
+    {
+        return $this->participantMaxCount;
+    }
+
+    public function setParticipantMaxCount(int $participantMaxCount): static
+    {
+        $this->participantMaxCount = $participantMaxCount;
+
+        return $this;
+    }
+}
